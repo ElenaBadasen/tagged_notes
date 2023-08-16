2f2ceb5b8fe01387364b3e9b6eafe9228c5e28ee
Lena Badasen <lena.badasen@gmail.com>
2023-08-16 19:37:47 +0100

    choose green and yellow tags

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,12 +5,36 @@ use serde::{Serialize, Deserialize};
 use chrono::prelude::*;
 
 #[tauri::command]
+fn all_tags() -> Vec<String> {
+    vec![
+        "tag1".to_string(),
+        "tag2".to_string(),
+        "tag3".to_string(),
+        "tag4".to_string(),
+        "tag5".to_string(),
+        "tag7".to_string(),
+        "tag77".to_string(),
+        "tag5353".to_string(),
+        "tag0000".to_string(),
+        "tag88888888888888".to_string(),
+        "tag1111111111".to_string(),
+        "tag11111".to_string(),
+        "tag156".to_string(),
+        "tag1333".to_string(),
+    ]
+}
+
+#[tauri::command]
 fn notes() -> Vec<Note> {
     vec![
-        Note{key: "1".to_string(), value: "note1".to_string()}, 
-        Note{key: "2".to_string(), value: "note2".to_string()}, 
-        Note{key: "3".to_string(), value: "loooooooooooooooong loooooooooooooooong note3".to_string()},
-        Note{key: "4".to_string(), value: format!("{:?}", Utc::now())},
+        Note{key: "1".to_string(), value: "note1".to_string(), 
+            tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string()]}, 
+        Note{key: "2".to_string(), value: "note2".to_string(), 
+            tags: vec!["tag3".to_string(), "tag2".to_string()]}, 
+        Note{key: "3".to_string(), value: "loooooooooooooooong loooooooooooooooong note3".to_string(), 
+            tags: vec!["tag7".to_string(), "tag2".to_string(), "tag3".to_string()]},
+        Note{key: "4".to_string(), value: format!("{:?}", Utc::now()), 
+            tags: vec!["tag1".to_string(), "tag2".to_string(), "tag3".to_string(), "tag398".to_string()] },
     ]
 }
 
@@ -18,11 +42,12 @@ fn notes() -> Vec<Note> {
 struct Note {
     key: String,
     value: String,
+    tags: Vec<String>,
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![notes])
+        .invoke_handler(tauri::generate_handler![notes, all_tags])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { Component } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import Icon from '@mdi/react';
+import { mdiRefresh } from '@mdi/js';
 
 type AppState = {
-  notes: Array<{key: React.Key, value: String}>,
+  notes: Array<{key: React.Key, value: String, tags: Array<String>}>,
+  all_tags: Array<String>,
+  yes_tags: Array<String>,
+  no_tags: Array<String>,
 }
 
 interface AppProps {
@@ -11,31 +16,101 @@ interface AppProps {
 class App extends Component<{}, AppState> {
   constructor (props: AppProps){
     super(props);
-    this.updateNotes = this.updateNotes.bind(this);
-  }
 
-  componentWillMount() {
-    this.setState({notes: []});
+    this.state = {
+      notes: [],
+      all_tags: [],
+      yes_tags: [],
+      no_tags: [],
+    };
+
+    this.updateNotes = this.updateNotes.bind(this);
+    this.setAllTags = this.setAllTags.bind(this);
+    this.handleTagLeftClick = this.handleTagLeftClick.bind(this);
+    this.handleTagRightClick = this.handleTagRightClick.bind(this);
+    this.handleTagClick = this.handleTagClick.bind(this);
   }
 
   componentDidMount() {
     this.updateNotes();
+    this.setAllTags();
   }
 
   updateNotes() {
-    console.log("HER1");
     let self = this;
     invoke("notes").then((result) => {
-      console.log("HERE2", result, self);
-      let notes = result as Array<{key: React.Key, value: String}>;
+      let notes = result as Array<{key: React.Key, value: String, tags: Array<String>}>;
       self.setState({notes: notes});
     });
+  }
+
+  setAllTags() {
+    let self = this;
+    invoke("all_tags").then((result) => {
+      let all_tags = result as Array<String>;
+      self.setState({all_tags: all_tags});
+    });
+  }
+
+  handleRefreshClick = (e: Event) => {
+    this.setState({
+      yes_tags: [],
+      no_tags: [],
+    });
+  }
+
+  handleTagClick = (e: Event) => {
+    if (e.type === 'click') {
+      this.handleTagLeftClick(e);
+    } else if (e.type === 'contextmenu') {
+      this.handleTagRightClick(e);
+    }
+  }
+
+  handleTagLeftClick(e: Event) {
+    let target = e.currentTarget as Element;
+    let value: String = target.textContent || "";
+    let yes_tags = this.state.yes_tags;
+    const index = yes_tags.indexOf(value, 0);
+    if (index > -1) {
+      yes_tags.splice(index, 1);
+    } else {
+      let no_tags = this.state.no_tags;
+      const index2 = no_tags.indexOf(value, 0);
+      if (index2 > -1) {
+        no_tags.splice(index2, 1);
+        this.setState({no_tags: no_tags});
+      }
+      yes_tags.push(value);
+    }
+    this.setState({yes_tags: yes_tags});
+    e.preventDefault();
+  }
+
+  handleTagRightClick(e: Event) {
+    let target = e.currentTarget as Element;
+    let value: String = target.textContent || "";
+    let no_tags = this.state.no_tags;
+    const index = no_tags.indexOf(value, 0);
+    if (index > -1) {
+      no_tags.splice(index, 1);
+    } else {
+      let yes_tags = this.state.yes_tags;
+      const index2 = yes_tags.indexOf(value, 0);
+      if (index2 > -1) {
+        yes_tags.splice(index2, 1);
+        this.setState({yes_tags: yes_tags});
+      }
+      no_tags.push(value);
+    }
+    this.setState({no_tags: no_tags});
+    e.preventDefault();
   }
 
   render() {
       return (
         <div className="block">
-        <div className="tabs is-centered is-large">
+        <div className="tabs is-centered is-large m-4">
           <ul>
             <li className="is-active"><a>Notes</a></li>
             <li><a>New</a></li>
@@ -44,8 +119,38 @@ class App extends Component<{}, AppState> {
         </div>
   
         <div className="container has-text-centered">
-          <div className="block">
-            {this.state.notes.map((d) => <li key={d.key}>{d.value}</li>)}
+          <div className="tags is-centered">
+            {this.state.all_tags.map((t) =>
+              <span className={"tag is-light is-medium is-clickable" + 
+                (this.state.yes_tags.includes(t) ? " is-primary" : (this.state.no_tags.includes(t) ? " is-warning" : ""))} 
+                onClick={this.handleTagClick} 
+                onContextMenu={this.handleTagClick}
+                key={t as React.Key}>
+                {t}
+              </span>
+            )}
+            <span key = "__refresh" className="m-1 is-clickable" onClick={this.handleRefreshClick} >
+              <Icon path={mdiRefresh} size={1} />
+            </span>
+            
+          </div>
+        </div>
+
+        <div className="container has-text-centered">
+          <div className="block has-text-left m-6">
+            {this.state.notes.map((d) => 
+              <div key={d.key} className="block">
+                <div className="block mb-4">
+                  {d.value}
+                </div>
+                <div className="tags">
+                  {d.tags.map((t) =>
+                    <span key={t as React.Key} className="tag is-light">
+                      {t}
+                    </span>
+                  )}
+                </div>
+              </div>)}
           </div>
           <button className="button" onClick={this.updateNotes}>Refresh</button>
         </div>
